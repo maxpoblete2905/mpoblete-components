afe1a7ced195dca78f13f114faa66d846a78b6af
max esteban poblete pizarro <max.poblete.externo@cmpc.cl>
2024-03-08 18:06:49 -0300

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import "./MyLabel.css";
+
+interface Props {
+  /**
+   * Text to display
+   */
+  label: string;
+
+  /**
+   * Label size
+   */
+  size?: "normal" | "h1" | "h2" | "h3";
+
+  /**
+   * all label caps latter
+   */
+  allCaps?: boolean;
+
+  /**
+   * color label text
+   */
+  color?: "text-primary" | "text-secondary" | "text-tertiary";
+
+  /**
+   * fontColor
+   */
+
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label,
+  size = "normal",
+  allCaps = false,
+  color = "text-primary",
+  fontColor = "",
+}: Props) => {
+  return (
+    <span className={`${size} ${color}`} style={{ color: fontColor }}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,50 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../components/MyLabel";
+
+const meta = {
+  title: "mi label",
+  component: MyLabel,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    size: {
+      control: "radio",
+    },
+  },
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    label: "basic",
+    size: "normal",
+  },
+};
+
+export const AllCaps: Story = {
+  args: {
+    label: "All Caps Label",
+    size: "normal",
+    allCaps: true,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: "Secondary label",
+    size: "normal",
+    color: "text-secondary",
+  },
+};
+
+export const CustomColor: Story = {
+  args: {
+    label: "Custom label",
+    size: "normal",
+    fontColor: "#ae23d8",
+  },
+};
