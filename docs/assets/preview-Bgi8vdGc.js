16705e1ae92b4803cd1f82a840fa860582540a78
max esteban poblete pizarro <max.poblete.externo@cmpc.cl>
2024-03-12 22:34:08 -0300

    test deploy

--- docs/assets/preview-Bgi8vdGc.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-xCxdVCNc.js","./iframe-BP2iPWIZ.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js","./react-18-iZ4baTDS.js","./index-D9uilScg.js","./index-cQil0Ac5.js","./index-Bw8VTzHM.js","./inheritsLoose-BqKIO1NE.js","./index-UvUotFBn.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-BP2iPWIZ.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-xCxdVCNc.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
