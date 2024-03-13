8638eef4a7c17321dc0e222d23a7d4b08407d904
max esteban poblete pizarro <max.poblete.externo@cmpc.cl>
2024-03-13 19:01:50 -0300

    set new deployd

---
+++ docs/assets/Input.stories-DvBCWU02.js
@@ -0,0 +1,10 @@
+import{C as r}from"./Input.component-COEWWkSA.js";import"./jsx-runtime-DQ32znRX.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./handlerThemeColor-Py96A-N_.js";import"./Form-BU-uEKGz.js";import"./ThemeProvider-BHT--WVz.js";import"./index-CpCSUgdU.js";import"./Col-DvNPdkmS.js";const v={title:"Component/Input",component:r,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{theme:{control:"select"},isneon:{control:"boolean"}}},e={args:{id:"username",isneon:!1,label:"Username",type:"text",value:"test",handleChange:a=>console.log(`Input value: ${a}`)}};var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    id: "username",
+    isneon: false,
+    label: "Username",
+    type: "text",
+    value: "test",
+    handleChange: (value: ChangeEvent<HTMLInputElement>) => console.log(\`Input value: \${value}\`)
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const I=["InputBasic"];export{e as InputBasic,I as __namedExportsOrder,v as default};
