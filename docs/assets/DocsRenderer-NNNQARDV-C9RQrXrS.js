1caafc09e968c8b19ea464a68e08383d6f87590c
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-03-17 19:33:26 -0300

    feat: new deployd fix filter date

--- docs/assets/DocsRenderer-NNNQARDV-C9RQrXrS.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./index-mlbUBBkt.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as p}from"./iframe-CbJ-Essa.js";import{R as e,r as c}from"./index-DH5ua8nC.js";import{r as l,u}from"./react-18-iZ4baTDS.js";import{C as h,A as E,H as d,D as x}from"./index-CU3MJSq4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D9uilScg.js";import"./index-Bw8VTzHM.js";import"./inheritsLoose-B7h9gheN.js";import"./index-UvUotFBn.js";import"./index-DrFu-skq.js";var _={code:h,a:E,...d},D=class extends c.Component{constructor(){super(...arguments),this.state={hasError:!1}}static getDerivedStateFromError(){return{hasError:!0}}componentDidCatch(t){let{showException:r}=this.props;r(t)}render(){let{hasError:t}=this.state,{children:r}=this.props;return t?null:e.createElement(e.Fragment,null,r)}},O=class{constructor(){this.render=async(t,r,o)=>{let n={..._,...r==null?void 0:r.components},s=x;return new Promise((m,i)=>{p(()=>import("./index-mlbUBBkt.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(({MDXProvider:a})=>l(e.createElement(D,{showException:i,key:Math.random()},e.createElement(a,{components:n},e.createElement(s,{context:t,docsParameter:r}))),o)).then(()=>m())})},this.unmount=t=>{u(t)}}};export{O as DocsRenderer,_ as defaultComponents};
