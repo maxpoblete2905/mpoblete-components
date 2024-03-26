81144549c6ea22506c2bc353bf86881ae459a00c
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-03-26 12:53:38 -0300

    deploys new version

---
+++ docs/assets/Input.stories-NYil1ouv.js
@@ -0,0 +1,10 @@
+import{C as s}from"./Input.component-Bce8lbtM.js";import"./jsx-runtime-DQ32znRX.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";const m={title:"Component/Input",component:s,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{theme:{control:"select"},isneon:{control:"boolean"}}},e={args:{id:"username",isneon:!1,label:"Username",type:"text",value:"test",handleChange:o=>console.log(`Input value: ${o}`)}};var n,t,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    id: "username",
+    isneon: false,
+    label: "Username",
+    type: "text",
+    value: "test",
+    handleChange: (value: ChangeEvent<HTMLInputElement>) => console.log(\`Input value: \${value}\`)
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const c=["InputBasic"];export{e as InputBasic,c as __namedExportsOrder,m as default};
