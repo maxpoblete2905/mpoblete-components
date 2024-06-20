f64d421a004a81f2433c232f01c5466f455787d3
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-06-20 14:32:45 -0400

    feat: set doc intro

---
+++ docs/assets/DocsRenderer-NNNQARDV-BslqmtOP.js
@@ -0,0 +1,1 @@
+import{D as n,d as f}from"./chunk-HLWAVYOI-CpM0Xuxk.js";import"./iframe-BByIbgua.js";import"../sb-preview/runtime.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./react-16-CNRis4zp.js";import"./index-DXimoRZY.js";import"./index-Dx4y3nsS.js";import"./index-DrFu-skq.js";export{n as DocsRenderer,f as defaultComponents};
--- docs/assets/DocsRenderer-NNNQARDV-Cc_94svx.js
+++
@@ -1,1 +0,0 @@
-import{D as n,d as f}from"./chunk-HLWAVYOI-CMhx-Ljo.js";import"./iframe-DGKP6jA-.js";import"../sb-preview/runtime.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./react-16-CNRis4zp.js";import"./index-DXimoRZY.js";import"./index-Dx4y3nsS.js";import"./index-DrFu-skq.js";export{n as DocsRenderer,f as defaultComponents};
